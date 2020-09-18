5b43bf4a38a2f9f2bec7a79670ff053b4610bd59
David Valachovic <David.E.Valachovic@gmail.com>
2020-09-18 15:47:45 -0500

    Updates

---
+++ precache-manifest.8d43afecdc1cbe60311ca7053a876a90.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694c2d106d9ec2ba88be7e138cc9c855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0bb9761a21d8d41416a",
+    "url": "/static/css/2.63ba52ef.chunk.css"
+  },
+  {
+    "revision": "77ac75fe65b762a6c3cb",
+    "url": "/static/css/main.aed65fc8.chunk.css"
+  },
+  {
+    "revision": "e0bb9761a21d8d41416a",
+    "url": "/static/js/2.da7a2536.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.da7a2536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ac75fe65b762a6c3cb",
+    "url": "/static/js/main.13ada2b1.chunk.js"
+  },
+  {
+    "revision": "3295169d05bec4654759",
+    "url": "/static/js/runtime-main.29d7a116.js"
+  },
+  {
+    "revision": "dc6d8942f14a3114726cfef9a3b5d9c3",
+    "url": "/static/media/logo_wide.dc6d8942.png"
+  },
+  {
+    "revision": "0fd7ed71f633e51d0cc552e355b60117",
+    "url": "/static/media/logo_wide_transparent.0fd7ed71.png"
+  }
+]);
